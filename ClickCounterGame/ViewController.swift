d836da4bf915bd93e87a041f11f63b61ab5cd017
alwin <alwin@spotify.com>
2018-08-07 11:26:40 -0400

    Initial Commit

---
+++ ClickCounterGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClickCounterGame
+//
+//  Created by Alwin Ventura on 8/7/18.
+//  Copyright © 2018 Alwin Ventura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
